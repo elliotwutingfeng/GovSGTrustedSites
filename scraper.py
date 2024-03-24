ec595eb9b511d35bf63ff5bc2c1ce911c8161764
Wu Tingfeng <wutingfeng@outlook.com>
2024-03-24 11:48:56 +0800

    Replace deprecated utcnow()

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import asyncio
 import logging
 import re
 import socket
-from datetime import datetime
+import datetime
 
 import aiohttp
 import cchardet  # type: ignore
@@ -116,7 +116,7 @@ def current_datetime_str() -> str:
     Returns:
         str: Timestamp in strftime format "%d_%b_%Y_%H_%M_%S-UTC"
     """
-    return datetime.utcnow().strftime("%d_%b_%Y_%H_%M_%S-UTC")
+    return datetime.datetime.now(datetime.UTC).strftime("%d_%b_%Y_%H_%M_%S-UTC")
 
 
 def clean_url(url: str) -> str:
